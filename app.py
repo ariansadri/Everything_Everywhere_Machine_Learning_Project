55ecaf6038045ab20941a3dda009ec8ce2e7c57f
zach steele <zacharysteele8@gmail.com>
2020-01-10 23:27:45 -0600

    pushing Machine learning Stuff

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import (
     Flask,
     render_template,
-    jsonify)
+    jsonify,
+    request)
 import pandas as pd
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
+model = pickle.load(open('SVCmodel.pkl', 'rb'))
 
 @app.route("/")
 def home_page():
@@ -19,6 +24,23 @@ def get_food():
     df.dropna(how='any', inplace=True)
     return jsonify(df.to_dict('records'))
 
+@app.route("/form", methods=["GET", "POST"])
+def send():
+    X1 = 0
+    X2 = 0
+    X3 = 0
+    if request.method == "POST":
+        X1 += int(request.form["Price"])
+        X2 += int(request.form["Rating"])
+        X3 += int(request.form["Popularity"])
+
+    Xnew = np.asarray([X1,X2,X3])
+    Xnew = Xnew.reshape(1,-1)
+    # Xnew_scalled = X_scaler.transform(Xnew)
+    ynew = model.predict(Xnew)
+    ynew_str = str(ynew[0])
+
+    return render_template('index.html', variable=ynew_str)
 
 if __name__ == "__main__":
     app.run(debug=True)
