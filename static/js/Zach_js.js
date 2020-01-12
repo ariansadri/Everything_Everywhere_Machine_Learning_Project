208805a59d4d96d53143e359100ff8c32ea7262e
zach steele <zacharysteele8@gmail.com>
2020-01-12 11:19:20 -0600

    pushing variable

--- static/js/Zach_js.js
+++ static/js/Zach_js.js
@@ -5,6 +5,14 @@
 
 // });
 
-d3.json('/get-food').then(function(data){
-    console.log(data);
+d3.csv('static/js/variable.csv').then(function(data){
+    console.log(data[0]);
+
+
+
+
+
+    
+
+    
 })
