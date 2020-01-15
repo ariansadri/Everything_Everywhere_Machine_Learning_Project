ac9c103b96b053998f7338355d01739dc36e7768
zach steele <zacharysteele8@gmail.com>
2020-01-14 19:29:25 -0600

    Changing Lower Case

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def get_food():
 @app.route('/weather')
 def weather():
 
-    return render_template("Weather.html")
+    return render_template("weather.html")
 
 
 @app.route("/form", methods=["GET", "POST"])
